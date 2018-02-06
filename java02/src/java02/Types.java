ad2036d45d31b9a39e9fc6e492d627c457003357
Anouk ZOUAOUI <anouk.zouaoui@anoukzouaoui.fr>
2018-02-06 10:01:22 +0100

    Exercice java02

---
+++ java02/src/java02/Types.java
@@ -0,0 +1,33 @@
+package java02;
+
+public class Types {
+
+	public static void main(String[] args) {
+		//Les entiers
+		int entier1 = 0;
+		System.out.println("int: " + entier1);
+		short entier2 = 1;
+		System.out.println("short: " + entier2);
+		byte entier3 = 2;
+		System.out.println("byte: " + entier3);
+		long entier4 = 3;
+		System.out.println("long: " + entier4);
+		
+		//Les décimaux
+		float decimal1 = 100.2f;
+		System.out.println("flaot: " + decimal1);
+		double decimal2 = 100.2d;
+		System.out.println("double: " + decimal2);
+		
+		//Booléen
+		boolean booleen = true;
+		System.out.println("booleen: " + booleen);
+		
+		//Caractères
+		char caractere = 'A';
+		System.out.println("char: " + caractere);
+		String chaineCaractere = "Bonjour";
+		System.out.println("String: " + chaineCaractere);
+	}
+
+}
