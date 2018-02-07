f7697cd1d16abc9407ed0f212f562b3e53abb8b9
Anouk ZOUAOUI <anouk.zouaoui@anoukzouaoui.fr>
2018-02-07 09:00:57 +0100

    Exercice java10e

---
+++ java10e/src/util/Compteur.java
@@ -0,0 +1,33 @@
+package util;
+
+/**
+ * Classe Compteur
+ * @author anoukzouaoui
+ *
+ */
+public class Compteur {
+	//Attribut privé cpt de la classe Compteur
+	private int cpt;
+	
+	/**
+	 * Fonction permettant d'incrémenter le compteur cpt
+	 */
+	public void increment() {
+		++ cpt;
+	}
+	
+	/**
+	 * Fonction permettant d'afficher le compteur cpt
+	 */
+	public void affiche() {
+		System.out.println("Valeur: " + cpt);
+	}
+	
+	/**
+	 * Fonction permettant de retourner le compteur
+	 * @return int cpt
+	 */
+	public int lireCount() {
+		return cpt;
+	}
+}
